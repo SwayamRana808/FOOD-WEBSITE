d7c18644f0e5f02437f173ec5efed7d1c8db9887
swayamName1 <swayamrana808@gmail.com>
2024-01-02 23:54:51 +0530

    last

--- src/components/Body.js
+++ src/components/Body.js
@@ -46,7 +46,7 @@ const Body=()=>{
                   <button  
                              onClick={handleSearch}
           
-                                        className="text-white min-w-[35px] grow bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg   text-center "><i class="fa fa-search"></i></button>
+                                        className="text-white min-w-[35px] grow bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg   text-center "><i className="fa fa-search"></i></button>
                 </div>
                 <button className="filter-btn mt-[20px] text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{cursor:"pointer"}} onClick={()=>{
                         const filter2=listOfRestuarant.filter((res)=>{
